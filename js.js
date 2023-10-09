cad8697736de717527bca3d7f2725ffd48edaebb
Nuradi <nuradi@gmail.com>
2023-10-09 14:53:56 +0300

    finished task 2

---
+++ js.js
@@ -0,0 +1,30 @@
+// const a = +prompt("Пишите число чтобы узнать Констант больше или меньше");
+// if (a>10) {
+//     console.log("Const Больше");
+// }else if(a===10){
+//     console.log("Const равно 10 :)");
+// }else{
+//     console.log("Const меншье");
+// }
+
+// let text = "lorem";
+// if (text==="lorem") {
+//     console.log(text);
+// }else{
+//     console.log("У вас ошибка в тексте", text);
+// }
+
+// let comment = "Lorem Ipsum i tak dalee ";
+// if (comment.length>120) {
+//     console.log("Тескт слишком длинный");
+// }else{
+//     console.log(comment);
+// }
+
+// let cartSum = 5000;
+// let skid = 0.05;
+// if (cartSum>=5000) {
+//     console.log(`Ваша скидка 5%. Итог скидки:${skid*cartSum}. Доставка бесплатно`);
+// }else if(cartSum<5000){
+//     console.log(`Итого к оплате: ${cartSum}. До скидки осталось: ${5000-cartSum}`);
+// }
